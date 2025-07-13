1ff9ad6accdc3049fd260aa4e7740ec11eb126bc
Maly Tona <tonagoodboy@gmail.com>
2025-07-13 14:23:34 +0700

    user & admin permission

--- app/Livewire/Auth/ForgotPassword.php
+++ app/Livewire/Auth/ForgotPassword.php
@@ -2,10 +2,29 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Password;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Forgot Password')]
 class ForgotPassword extends Component
 {
+    public $email;
+
+    public function save()
+    {
+        $this->validate([
+            'email' => 'required|email|exists:users,email|max:255'
+
+        ]);
+
+        $status = Password::sendResetLink(['email' => $this->email]);
+
+        if ($status === Password::RESET_LINK_SENT) {
+            session()->flash('success', 'Password reset link បានផ្ញើទៅអ៊ីមែលរបស់អ្នករួចហើយ');
+            $this->email = '';
+        }
+    }
     public function render()
     {
         return view('livewire.auth.forgot-password');
