f9c4995d5aed879a2209f38d8cc124e5afe104c7
Maly Tona <tonagoodboy@gmail.com>
2025-07-28 17:16:59 +0700

    add new AdminUserSeeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -5,17 +5,27 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-        ]);
-        
+        // Check the 'admin' role exists
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        // Use firstOrCreate to prevent errors on re-run
+        $adminUser = User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                // Use env() for a secure password
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'password')),
+                'email_verified_at' => now(),
+            ]
+        );
+
+
+        $adminUser->assignRole($adminRole);
     }
 }
